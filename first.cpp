c574416f40af54dbaac47bf5276032afdc551b2d
gauravsewatkar <sewatkargaurav2000@gmail.com>
2021-03-05 23:44:04 +0530

    first.cpp is commited

---
+++ first.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+int a=10,b=12,z,x;
+float f=5.5;
+char d='A';
+double e=3.456;
+
+cout<<a<<endl;
+a=a+b;
+cout<<a<<" " <<f<<" " <<d<<" " <<e<<endl;
+
+cout<<"enter the number";
+cin>>z;
+x=z+b;
+cout<<x;
+
+return 0;
+}
+
